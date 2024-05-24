feadbb3bd854d48cdb4ba48db8ae394191f5dc0c
dhanu421366 <nimbalkardhananjay349@gmail.com>
2024-05-24 17:59:58 +0530

    first commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+/* The CSS code you provided is styling the layout and appearance of elements in a web page. Here's a
+breakdown of what each part of the code is doing: */
+import { useState } from 'react'
+function App() {
+  return (
+    <>
+    <h1>hallo Dhanu </h1>
+    </>
+  )
+}
+
+export default App
