fbeffd83586fdbb6a0fff30bc74590c6b5860afd
dhanu421366 <nimbalkardhananjay349@gmail.com>
2024-05-24 21:45:38 +0530

    first commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,24 @@
-/* The CSS code you provided is styling the layout and appearance of elements in a web page. Here's a
-breakdown of what each part of the code is doing: */
 import { useState } from 'react'
+import { BrowserRouter ,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Project from './pages/Project'
 function App() {
   return (
     <>
-    <h1>hallo Dhanu </h1>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path='/sign-up' element={<SignUp/>}/>
+    <Route path='/sign-in' element={<SignIn/>}/>
+    <Route path='/dashboard' element={<Dashboard/>}/>
+    <Route path='/project' element={<Project/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
